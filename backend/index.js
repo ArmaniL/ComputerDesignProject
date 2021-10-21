b48ad921685a8ed840a56a7ef3d34954456f47c0
ArmaniL <35434507+ArmaniL@users.noreply.github.com>
2021-10-21 19:24:42 -0400

    Added front end and backend

---
+++ backend/index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const exec = require("child_process").exec;
+exec("rm temp/*");
+const random_words = [
+  "Fork",
+  "Freeway",
+  "Fruit",
+  "Fungus",
+  "Game",
+  "Garden",
+  "Gas",
+  "Gate",
+];
+const random_key = () => {
+  const first = random_words[Math.ceil(Math.random() * 7)];
+  const second = random_words[Math.ceil(Math.random() * 7)];
+  return first + "-" + second;
+};
+const codeDirectory = {};
+const app = express();
+const port = 3000;
+
+app.use(fileUpload());
+app.use(express.static("public"));
+app.post("/upload", (req, res) => {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send("No files were uploaded.");
+  }
+  const { upload } = req.files;
+  const uploadPath = __dirname + "/temp/" + upload.name;
+  upload.mv(uploadPath, function (err) {
+    if (err) return res.status(500).send(err);
+    const key = random_key();
+    codeDirectory[key] = uploadPath;
+    res.send("File uploaded! Your file code is:" + key);
+  });
+});
+app.use(express.urlencoded(
+    {
+        extended:true
+    }
+))
+app.post("/download",(req,res)=>{
+    const {upload } =req.body
+    if (codeDirectory[upload]){
+      
+        res.download(codeDirectory[upload], function(error){
+            if(error){
+            console.log("Error : ", error)
+            }
+        });
+    }
+    else{
+        return res.status(400).send("Invalid Code");
+    }
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,18 +1,45 @@
 <script>
-	
-	let clickedupload=false;
-	let clickeddownload = false;
-	
+  let clickedupload = false;
+  let clickeddownload = false;
+  const server = document.URL;
+  function upload() {
+    clickedupload = true;
+    console.log("upload");
+  }
+  function download() {
+    clickeddownload = true;
+  }
 </script>
 
 <h1>File Sharing</h1>
-<img on:click={( )=>{}} alt ="upload"src={"cloud-data-upload.svg"} >
-<img on:click={( )=>{}} alt ="download" src ={"download-file-round-line.svg"}>
-<style>
+{#if clickeddownload}
+<h1>File Download</h1>
+<form action={server + "download"} method="post" >
+  <input type="submit" value="Upload" />
+  <input name="upload" type="text" />
+</form>
+{:else if clickedupload}
+  <h1>File Upload</h1>
+  <form action={server + "upload"} method="post" encType="multipart/form-data">
+    <input type="submit" value="Upload" />
+    <input name="upload" type="file" />
+  </form>
+{:else}
+  <img on:click={upload} alt="upload" src={"cloud-data-upload.svg"} />
+  <img
+    on:click={download}
+    alt="download"
+    src={"download-file-round-line.svg"}
+  />
+{/if}
 
-	img{
-		width: 10rem;
-		margin: auto;
-		padding-right: 10rem;
-	}
+<style>
+  body {
+    background-color: beige;
+  }
+  img {
+    width: 10rem;
+    margin: auto;
+    padding-right: 10rem;
+  }
 </style>
