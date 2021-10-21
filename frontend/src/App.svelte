9bddcb78d144381260eaa5c57492c20cc3a478c1
ArmaniL <35434507+ArmaniL@users.noreply.github.com>
2021-10-21 16:41:44 -0400

    First commit

---
+++ frontend/src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+	
+	let clickedupload=false;
+	let clickeddownload = false;
+	
+</script>
+
+<h1>File Sharing</h1>
+<img on:click={( )=>{}} alt ="upload"src={"cloud-data-upload.svg"} >
+<img on:click={( )=>{}} alt ="download" src ={"download-file-round-line.svg"}>
+<style>
+
+	img{
+		width: 10rem;
+		margin: auto;
+		padding-right: 10rem;
+	}
+</style>
